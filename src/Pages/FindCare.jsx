51f002c71c36f376fcb12d5a7ef7562ddcd45767
teecrown <teecrown@gmail.com>
2025-07-30 05:00:15 +0100

    s

--- src/Pages/FindCare.jsx
+++ src/Pages/FindCare.jsx
@@ -97,10 +97,10 @@ const MedicalSearch = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
+    <div className="min-h-screen bg-gray-100 ">
         <Navbar />
       {/* Header & Search */}
-      <div className="text-center mb-6">
+      <div className="text-center my-6">
         <h1 className="text-3xl font-bold mb-3">Find Medical care</h1>
         <div className="flex justify-center items-center max-w-xl mx-auto bg-white border rounded-md px-3 py-2 shadow">
           <input
--- src/Pages/SupportPage.jsx
+++ src/Pages/SupportPage.jsx
@@ -1,15 +1,32 @@
 import React from "react";
-// import Navbar from "../Components/Navbar";
+import Navbar from "../Components/Navbar";
 import FindCare from "./Healthtips.jsx";
 import Footer from "../Components/Footer";
 import FAQ from "../Components/FAQ";
+import support from "../assets/support-hero.jpg";
 
 const SupportPage = () => {
   return (
     <div>
-      
-      <FindCare />
-
+      <Navbar />
+      <div>
+        <img src={support} alt="" className='absolute w-full  h-[201px] sm:h-[313px] ' />
+      </div>
+          
+      <div className=" relative  flex justify-center  items-center border w-[620px] p-4 ">
+        <div >
+        <h2 className="text-white leading-[100%] font-700 text-[45px]   text-center justify-center items-center w-[582px] h-[122px] ">
+          {" "}
+          We're <span className='text-[#0B9A55]'> Here to Help</span>, Every Step of the Way{" "}
+        </h2>
+        <p className="text-center text-white text-[18px] leading-[150%] font-500 w-[600px] h-[72px]">
+          {" "}
+          Have questions, concerns, or need guidance? The Health Connect support
+          team is committed to helping you find answers quickly. Your peace of
+          mind matters to us.
+        </p>
+        </div>
+      </div>
       <FAQ />
       <Footer />
     </div>
